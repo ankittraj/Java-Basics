509f29360919902da963c931a5accd02e766cd98
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-10 21:59:48 +0530

    Add files via upload

---
+++ zShape.java
@@ -0,0 +1,9 @@
+//Print Z shape in java
+import java.util.*;
+
+public class zShape{
+    public static void main(String args[]){
+
+        System.out.println("*****\n   *\n  *\n *\n*****");
+    }
+}
